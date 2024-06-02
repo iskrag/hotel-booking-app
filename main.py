c83b100f08eab86bdb98a213d23bc0324165c1e0
iskra <iskrag@gmail.com>
2024-06-03 00:23:08 +0200

    Add credit card feature

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 df = pd.read_csv('hotels.csv', dtype={'id': str})
+df_cards = pd.read_csv('cards.csv', dtype=str).to_dict(orient='records')
 
 
 class Hotel:
@@ -36,14 +37,29 @@ class ReservationConfirmation:
         return content
 
 
+class CreditCard:
+    def __init__(self, number):
+        self.number = number
+
+    def validate(self, expiration, holder, cvc):
+        card_data = {'number': self.number, 'expiration': expiration,
+                     'holder': holder, 'cvc': cvc}
+        if card_data in df_cards:
+            return True
+
+
 print(df)
 hotel_ID = input('Enter the id of the hotel: ')
 hotel = Hotel(hotel_ID)
 
 if hotel.available():
-    hotel.book()
-    name = input('Enter your name: ')
-    reservation_ticket = ReservationConfirmation(name, hotel)
-    print(reservation_ticket.generate())
+    credit_card = CreditCard('1234567891234567')
+    if credit_card.validate('12/26', 'JOHN SMITH', '123'):
+        hotel.book()
+        name = input('Enter your name: ')
+        reservation_ticket = ReservationConfirmation(name, hotel)
+        print(reservation_ticket.generate())
+    else:
+        print('There was a problem with your payment.')
 else:
     print('Hotel is fully booked.')
